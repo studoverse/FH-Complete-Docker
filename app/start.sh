33c6e08708a791e38c4001405260b891e0833fcb
Zoltan Sasvari <zoltan.sasvari@studo.co>
2019-01-23 16:21:45 +0100

    better shutdown

--- app/start.sh
+++ app/start.sh
@@ -3,9 +3,17 @@
 echo "Starting apache"
 service apache2 restart # Apache won't start for some reason so start here
 
+# Handle docker shutdown gracefully
+cleanup () {
+  kill -s SIGTERM $!
+  exit 0
+}
+trap cleanup SIGINT SIGTERM
+
 # We need this so the container stays alive
 echo "Entering sleep"
 while [[ 1 ]]
 do
-    sleep 3600
+    sleep 60 &
+    wait $!
 done
